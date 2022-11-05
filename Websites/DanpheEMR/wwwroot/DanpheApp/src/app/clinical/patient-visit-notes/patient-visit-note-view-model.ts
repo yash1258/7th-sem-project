da916e089e13e9d02e1d11fe4d90469f085857fc
orphis <anrahya@orphis.local>
2022-11-05 09:40:39 +0530

    first

---
+++ Websites/DanpheEMR/wwwroot/DanpheApp/src/app/clinical/patient-visit-notes/patient-visit-note-view-model.ts
@@ -0,0 +1,11 @@
+export class PatientVisitNoteVM{
+    public patVisitNote:any;
+    public vitalsList:Array<any>;
+    public labRequisitionList:Array<any>;
+    public imagingRequisitionList:Array<any>;
+    public otherOrderList:Array<any>;
+    public homeMedicationList:Array<any>;
+    public diagnosisList:Array<any>;
+    public bodyPainList:Array<any>;
+    public procedureList:Array<any>;
+  }
---
+++ Websites/DanpheEMR/wwwroot/DanpheApp/src/app/clinical/shared/icd10.model.ts
@@ -0,0 +1,7 @@
+﻿export class ICD10 {
+    public ICD10Code: string = "";
+    public ICD10Description: string = "";
+    public ICD10ID: number = 0;
+    public ICDShortCode: string = "";
+    public ValidForCoding: boolean = true;
+}
